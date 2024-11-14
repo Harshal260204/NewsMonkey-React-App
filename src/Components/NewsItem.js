4835b714b6b85f406e5a5b2f097a444e059bc095
Harshal260204 <harshalingale26@gmail.com>
2024-11-15 01:21:43 +0530

    Used this.props And this.state

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
+
+  
+
   render() {
+
+    let {title, description, imageUrl}=this.props
     return (
       <div>
-        This Is A News Item Component
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/NewsDetail" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
