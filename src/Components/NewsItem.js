b15c8833012186b2d11de358ec7c4b5555a970b8
Harshal260204 <harshalingale26@gmail.com>
2024-11-15 22:25:10 +0530

    Used FetchAPI For Fetching The Data

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -12,13 +12,13 @@ export default class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{ width: "20rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://fortune.com/img-assets/wp-content/uploads/2024/07/Polymarket-Shayne-Coplan.jpg?resize=1200,600":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href="/NewsDetail/" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
